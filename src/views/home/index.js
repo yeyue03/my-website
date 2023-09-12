2bc1ff72f1eff0ddc37caedd0d8d5828d094868e
LG-PC-045\Administrator <632491231@qq.com>
2023-09-12 15:54:43 +0800

    feat: 首页整屏滚动

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,48 @@
-const Home = () => {
-  return (
-    <div className="home-wrap">
-      测试aaaaa
-    </div>
-  )
+import React, { Component } from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+import './home.less';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-wrap">
+        <ReactFullpage
+          //上面的参数设置都写在这里，不用再写在构造函数里面
+          render={() => {
+            return (
+              <ReactFullpage.Wrapper>
+                <div className="section">
+                  <div className="content-box">
+
+                  </div>
+                  {/* <img className="img" src="http://img.netbian.com/file/2023/0815/002805YxY24.jpg" alt="" /> */}
+                </div>
+
+                <div className="section">
+                  <div className="content-box">
+
+                  </div>
+                  {/* <img className="img" src="http://img.netbian.com/file/2023/0428/001406UwUZU.jpg" alt="" /> */}
+                </div>
+                <div className="section">
+                  <div className="content-box"></div>
+                  {/* <img className="img" src="http://img.netbian.com/file/2023/0819/115237KxFBw.jpg" alt="" /> */}
+                </div>
+                <div className="section">
+                  <div className="content-box"></div>
+                  {/* <img className="img" src="http://img.netbian.com/file/2023/0809/122616M46GH.jpg" alt="" /> */}
+                </div>
+                <div className="section">
+                  <div className="content-box"></div>
+                  {/* <img className="img" src="http://img.netbian.com/file/2023/0823/1045364lGTg.jpg" alt="" /> */}
+                </div>
+              </ReactFullpage.Wrapper>
+            );
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Home;
